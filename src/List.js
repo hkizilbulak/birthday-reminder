f3266bbb26a9b2ca59197a2aeb8d1d2bad488524
huseyin <huseyin.kizilbulak@volantx.com>
2022-08-01 18:37:31 +0300

    initial commit

---
+++ src/List.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const List = ({ people }) => {
+  return people.map((person) => (
+    <article key={person.id} className="person">
+      <img src={person.image} alt={person.name} />
+      <div>
+        <h4>{person.name}</h4>
+        <p>{person.age} years</p>
+      </div>
+    </article>
+  ));
+};
